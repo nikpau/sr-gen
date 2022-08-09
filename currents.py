9fa98eb8ba4cb18767da26c9834e149b26062d77
nikpau <n.paulig@web.de>
2022-08-09 09:48:17 +0200

    current map as class

--- currents.py
+++ currents.py
@@ -1,23 +1,37 @@
+from dataclasses import dataclass
 from typing import TypeVar
-from matplotlib import cm
-
-import numpy as np 
+import numpy as np
 import mesh
-import depth
-import matplotlib.pyplot as plt
 
-CurrentMap = TypeVar("CurrentMap")
+@dataclass
+class CurrentMap:
+    """Current map.
+    """
+    x: np.ndarray
+    y: np.ndarray 
 
-def current_map(m: mesh.Segment) -> CurrentMap:
+def current_map(m: mesh.Segment, v: float) -> CurrentMap:
+    """Generate a current map for a given mesh.
+    Current direction follows a sinusoidal pattern
+    over the length of the segment. Speed ranges
+    from -v to +v
+
+    Args:
+        m (mesh.Segment): Segment to generate current map for.
+        v (float): maximum speed of the current
+
+    Returns:
+        CurrentMap: Curren of same x or y shape as imput segment.
+    """
     
     ones= np.ones_like(m.yy)
     xout, yout = np.empty_like(ones), np.empty_like(ones)
 
-    lin = np.linspace(-2,2,ones.shape[0])
+    lin = np.linspace(-v,v,ones.shape[0])
     lin = list(map(np.sin,lin))
 
     for row in range(ones.shape[0]):
         xout[row] = ones[row] * lin[row]
         yout[row] = ones[row] * -lin[row]
 
-    return xout,yout
+    return CurrentMap(xout, yout)
