752d2f91085de716778459aae43a27142cf828a0
nikpau <n.paulig@web.de>
2022-08-09 13:19:40 +0200

    include package to tests

--- src/rivergen/tests.py
+++ src/rivergen/tests.py
@@ -1,13 +1,11 @@
-import mesh
-import depth
-import currents
+import rivergen as rg
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
 
-m = mesh.generate(30)
-d = depth.depth_map(m,2)
-c = currents.current_map(m,2)
+m = rg.mesh.generate(10)
+d = rg.depth.depth_map(m,1)
+c = rg.currents.current_map(m,1.5)
 
 #plt.scatter(m.xx,m.yy,c=d,cmap=cm.gray)
 plt.contourf(m.xx,m.yy,d,cmap=cm.ocean,levels = np.linspace(0,np.max(d),20))
