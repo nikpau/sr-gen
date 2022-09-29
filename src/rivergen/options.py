63c8b5838348f94fc770c847eaf0200ec823d21b
nikpau <n.paulig@web.de>
2022-09-29 23:11:16 +0200

    remove unused options

--- src/rivergen/options.py
+++ src/rivergen/options.py
@@ -1,7 +1,6 @@
 # Breadth and spacing of segments
 GP = 76 #  No. of grid points per segment width
 BPD = 20 # distance between gridpoints [m]
-BREADTH = ((GP-1)*BPD) # [m]
 
 # Print information about the generation 
 # process to stdout
