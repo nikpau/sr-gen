824f1b87d69ea0761f6469b444545591e7803670
nikpau <n.paulig@web.de>
2024-02-21 07:44:10 +0100

    version

--- src/rivergen/__init__.py
+++ src/rivergen/__init__.py
@@ -2,7 +2,7 @@ from .export import export_to_file
 from .config import ConfigFile
 from . import tests
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 __author__ = "Niklas Paulig <niklas.paulig@tu-dresden.de>"
 __all__ = ["export_to_file"]
 
