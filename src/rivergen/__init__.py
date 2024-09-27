b16e3f728a4452c86d6ed7a6c9353e38e5d0981b
nikpau <n.paulig@web.de>
2024-09-27 13:09:05 +0200

    version

--- src/rivergen/__init__.py
+++ src/rivergen/__init__.py
@@ -1,7 +1,7 @@
 from .utils import ConfigFile
 from . import tests
 
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 __author__ = "Niklas Paulig <niklas.paulig@tu-dresden.de>"
 __all__ = ["ConfigFile","tests"]
 
